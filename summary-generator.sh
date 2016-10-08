967cb39b44ff4e663adb92ad8c9ae8fff59a75dd
yonghong <yonghong.chenn@gmail.com>
2016-10-08 02:08:35 -0400

    first attempt to copy all cpp code to readme file

--- summary-generator.sh
+++ summary-generator.sh
@@ -1,7 +1,8 @@
-rm SUMMARY.md
+> SUMMARY.md
 touch SUMMARY.md
 
-fileIgnore=("README.md" "solution.cpp" "test.in")
+#fileIgnore=("README.md" "solution.cpp" "test.in")
+fileIgnore=("README.md" "test.in")
 
 generateMenu(){
     local dir=$1
@@ -16,6 +17,7 @@ generateMenu(){
         filename=`basename $file`
 
         if [[ -d "$file" ]]; then
+            > "$file/README.md"
             touch "$file"/README.md
             echo "$indent* [$filename]($file/README.md)" >> SUMMARY.md
             generateMenu $file "1+$depth"
@@ -23,7 +25,14 @@ generateMenu(){
             if [[ "${fileIgnore[@]}" =~ $filename ]]; then
                 continue
             fi
-            echo "$indent* [$filename]($file)" >> SUMMARY.md
+            #echo "$indent* [$filename]($file)" >> SUMMARY.md
+            # cat all files under same directory
+            local filedir=$(dirname "$file")
+            local value=`cat $file`
+            echo "{% raw %}" >> "$filedir/README.md"
+            echo "$value" >> "$filedir/README.md"
+            echo "{% endraw %}" >> "$filedir/README.md"
+            sed -i 's/^/    /' "$filedir/README.md"
         fi
     done
 }
